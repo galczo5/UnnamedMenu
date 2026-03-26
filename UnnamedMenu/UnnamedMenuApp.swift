85447bc9938f7df3e5503753ed90c7dfe2b3b1cc
galczo5 <kamil.galek@gmail.com>
2026-03-26 23:33:39 +0100

    Initial Commit

---
+++ UnnamedMenu/UnnamedMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnnamedMenuApp.swift
+//  UnnamedMenu
+//
+//  Created by Kamil on 26/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct UnnamedMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
